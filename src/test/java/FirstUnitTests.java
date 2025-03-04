18a1cfb2db5e10dbcdbfc41d7cd0becaef1af430
ValeryKharkov <valery@example.com>
2025-03-04 23:00:38 +0300

    Создан тест sumNumbersTests

--- src/test/java/FirstUnitTests.java
+++ src/test/java/FirstUnitTests.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FirstUnitTests {
+    @Test
+    void sumNumbersTests() {
+        /// AAA -> Arrange-Act-Assert
+        int a = 3;
+        int b = 4;
+
+        int expectedResult = 7;
+        int actualResult = a + b;
+
+        assertEquals(expectedResult, actualResult);
+    }
 }
